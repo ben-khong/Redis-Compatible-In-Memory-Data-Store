8a07fa63ec7bb9380fce3abd937b8e97db647cb1
Benjamin Khong <benkhong27@gmail.com>
2026-01-28 16:43:01 -0800

    Handle concurrent clients

--- app/main.go
+++ app/main.go
@@ -22,12 +22,17 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleThisClient(conn)
 	}
+}
 
+func handleThisClient(conn net.Conn) {
 	// Buffer to read incoming data
 	buf := make([]byte, 1024)
 
